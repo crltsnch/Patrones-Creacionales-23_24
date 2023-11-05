ffd3cb171c6df26bafd904b6ac0adb893bd82e24
crltsnch <crltsnch@myuax.com>
2023-11-05 12:27:51 +0100

    cambios

--- ejercicio 1/data.py
+++ ejercicio 1/data.py
@@ -7,16 +7,19 @@ data = pd.read_csv(URL, sep=';', encoding='ISO-8859-1')
 #print(data.head())  # Mostrar las primeras filas para visualizar los datos
 
 
+#Verificar si hay filas duplicadas
+#print(data.duplicated().sum())
+
 #Eliminamos las columnas vacías y columnas que no me vayan a servir
-colum_eliminar = ["PRECIO", "DIAS-EXCLUIDOS", "DESCRIPCION", "AUDIENCIA", "Unnamed: 29", "COORDENADA-Y", "COORDENADA-X", "LATITUD", "LONGITUD", "CONTENT-URL"]
-data = data.drop(colum_eliminar, axis=1)
-#print(data.head())
+colum_eliminar = ["PRECIO", "DIAS-EXCLUIDOS", "DESCRIPCION", "AUDIENCIA", "Unnamed: 29", "COORDENADA-Y", "COORDENADA-X", "LATITUD", "LONGITUD", "CONTENT-URL", "URL-ACTIVIDAD", "URL-INSTALACION"]
+data_final = data.drop(colum_eliminar, axis=1)
+#print(data_final.head())
 
 
 #En la columna "TIPO" queremos quedarnos con el tipo especifico de actividad por lo que vamos a dividir y quedarnos con lo que hay despues de la ultima /
 data["TIPO"] = data["TIPO"].str.split("/")
-data["TIPO"] = data["TIPO"].str[-1]
-#print(data.head())
+data_final["TIPO"] = data["TIPO"].str[-1]
+#print(data_final.head())
 
 
 #Tipos de datos que tenemos
@@ -24,8 +27,13 @@ data["TIPO"] = data["TIPO"].str[-1]
 
 
 #Fecha, fecha-fin y hota son object por lo que vamos a convertirlos a datetime. Vamos a convertir FECHA, FECHA-FIN y HORA a formato fecha
-data["FECHA"] = pd.to_datetime(data["FECHA"])
-data["FECHA-FIN"] = pd.to_datetime(data["FECHA-FIN"])
-data["HORA"] = pd.to_datetime(data["HORA"], format="%H:%M")
+data_final["FECHA"] = pd.to_datetime(data["FECHA"])
+data_final["FECHA-FIN"] = pd.to_datetime(data["FECHA-FIN"])
+data_final["HORA"] = pd.to_datetime(data["HORA"], format="%H:%M")
 
-print(data.head())
+
+print(data_final.head())
+#print(data_final.dtypes)
+
+#guardamos el data final en un csv en la carpeta data
+data_final.to_csv("/Users/carlotasanchezgonzalez/Documents/class/Patrones-Creacionales-23_24/ejercicio 1/data/data_final.csv", sep=";", index=False)
