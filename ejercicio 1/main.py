f7724523141d709ee6254dac8bbea8ad7a52863e
crltsnch <crltsnch@myuax.com>
2023-11-05 12:52:01 +0100

    clase factoria abstracta declarando productos abstractos

--- ejercicio 1/main.py
+++ ejercicio 1/main.py
@@ -2,21 +2,17 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 
-class AbstractFactory(ABC):
+class SamurAbstractFactory(ABC):
     """
-    The Abstract Factory interface declares a set of methods that return
-    different abstract products. These products are called a family and are
-    related by a high-level theme or concept. Products of one family are usually
-    able to collaborate among themselves. A family of products may have several
-    variants, but the products of one variant are incompatible with products of
-    another.
+    The Samur Abstract Factory interface declares a set of methods that return
+    different abstract products.
     """
     @abstractmethod
-    def create_product_a(self) -> AbstractProductA:
+    def realizarAnalisis(self) -> AbstractAnalisis:
         pass
 
     @abstractmethod
-    def create_product_b(self) -> AbstractProductB:
+    def mostrarVisualizacion(self) -> AbstractVisualizacion:
         pass
 
 
