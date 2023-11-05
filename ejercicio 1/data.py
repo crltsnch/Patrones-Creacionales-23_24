c0532d160b4716e76ed5d68b09749bfa7a8f07de
crltsnch <crltsnch@myuax.com>
2023-11-05 12:09:16 +0100

    limpieza: cambiar tipo de dato

--- ejercicio 1/data.py
+++ ejercicio 1/data.py
@@ -4,14 +4,28 @@ URL = "https://datos.madrid.es/egob/catalogo/212504-0-emergencias-activaciones.c
 
 # Leer CSV desde la URL
 data = pd.read_csv(URL, sep=';', encoding='ISO-8859-1')
-print(data.head())  # Mostrar las primeras filas para visualizar los datos
+#print(data.head())  # Mostrar las primeras filas para visualizar los datos
 
 
 #Eliminamos las columnas vacías y columnas que no me vayan a servir
-colum_eliminar = ["PRECIO", "DIAS-EXCLUIDOS", "DESCRIPCION", "AUDIENCIA", "Unnamed: 29", ]
+colum_eliminar = ["PRECIO", "DIAS-EXCLUIDOS", "DESCRIPCION", "AUDIENCIA", "Unnamed: 29", "COORDENADA-Y", "COORDENADA-X", "LATITUD", "LONGITUD", "CONTENT-URL"]
 data = data.drop(colum_eliminar, axis=1)
-print(data.head())
+#print(data.head())
 
-#En la columna "TIPO" queremos quedarnos con el tipo especifico de actividad por lo que vamos a quedarnos con lo que hay despues de la ultima /
-data["TIPO"] = data["TIPO"].str.split("/").str[-1]
+
+#En la columna "TIPO" queremos quedarnos con el tipo especifico de actividad por lo que vamos a dividir y quedarnos con lo que hay despues de la ultima /
+data["TIPO"] = data["TIPO"].str.split("/")
+data["TIPO"] = data["TIPO"].str[-1]
+#print(data.head())
+
+
+#Tipos de datos que tenemos
+#print(data.dtypes)
+
+
+#Fecha, fecha-fin y hota son object por lo que vamos a convertirlos a datetime. Vamos a convertir FECHA, FECHA-FIN y HORA a formato fecha
+data["FECHA"] = pd.to_datetime(data["FECHA"])
+data["FECHA-FIN"] = pd.to_datetime(data["FECHA-FIN"])
+data["HORA"] = pd.to_datetime(data["HORA"], format="%H:%M")
+
 print(data.head())
