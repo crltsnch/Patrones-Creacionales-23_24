e6143876d18e4f38e70a96c1e11630e2c3f14cb7
crltsnch <crltsnch@myuax.com>
2023-11-08 22:50:25 +0100

    creo un fichero para generar las opciones y las recomendaciones

---
+++ Ejercicio 2/logica_de_recomendacion.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = pd.read_csv('/Users/carlotasanchezgonzalez/Documents/class/Patrones-Creacionales-23_24/Ejercicio 2/data/data_final.csv')
+
+class RecomendacionOpcion:
+    def __init__
