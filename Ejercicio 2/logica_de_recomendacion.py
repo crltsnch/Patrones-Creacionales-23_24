7f4020918544bcd2990119338862439e2e3baac2
crltsnch <crltsnch@myuax.com>
2023-11-08 23:19:01 +0100

    opciones masa

--- Ejercicio 2/logica_de_recomendacion.py
+++ Ejercicio 2/logica_de_recomendacion.py
@@ -3,4 +3,14 @@ import pandas as pd
 data = pd.read_csv('/Users/carlotasanchezgonzalez/Documents/class/Patrones-Creacionales-23_24/Ejercicio 2/data/data_final.csv')
 
 class RecomendacionOpcion:
-    def __init__
+    def __init__(self):
+        pass
+
+    def masa_opcion(self, masa_seleccionada):
+        masas = data["tipo_masa"].unique()
+        if masa_seleccionada in masas:
+            return masa_seleccionada
+        else:
+            return "masa no disponible"
+    
+    
